ea441a86527d23bce467d76da5d7f25eb830715c
unknown <yesseiramartinez@gmail.com>
2025-10-06 15:57:01 -0600

    Script de migración de admins a MongoDB

---
+++ migrar_admins_mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://yesseiramartinez_db_user:NXp6YuexAdXAROrj@cluster0.uuwrtvt.mongodb.net/zonagamer?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  role: String
+});
+const Admin = mongoose.model('Admin', adminSchema);
+
+const admins = [
+  { username: 'Hector', password: '24681012', role: 'admin' },
+  { username: 'Larry', password: 'Lavn180524', role: 'admin' }
+];
+
+Admin.insertMany(admins)
+  .then(() => {
+    console.log('Admins migrados a MongoDB correctamente');
+    mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error('Error al migrar admins:', err);
+    mongoose.disconnect();
+  });
